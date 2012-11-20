f18fd4ec9d19e33a5943c3424f32315690835062
Micah Anderson <micah@riseup.net>
2012-11-20 13:49:20 -0500

    setup vagrant low security ssh public key to be able to connect as root

--- wheezy/vagrant.sh
+++ wheezy/vagrant.sh
@@ -12,6 +12,14 @@ curl -Lo /home/vagrant/.ssh/authorized_keys \
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:vagrant /home/vagrant/.ssh
 
+# setup vagrant low security ssh public key to be able to connect as root
+mkdir /root/.ssh
+cp /home/vagrant/.ssh/authorized_keys /root/.ssh
+chown -R root:root /root/.ssh
+chmod 0700 /root/.ssh
+chmod 0600 /root/.ssh/authorized_keys
+
+
 # Customize the message of the day
 echo 'Welcome to your Vagrant-built virtual machine.' > /var/run/motd
 
