66bc17aa1d714994396b3970a830d8f30e0ceaeb
varac <varacanero@zeromail.org>
2015-04-25 21:12:53 +0200

    fixed typo in base.sh

--- definitions/wheezy/base.sh
+++ definitions/wheezy/base.sh
@@ -20,7 +20,7 @@ sed -i -e 's/%sudo.*ALL=(ALL:ALL) ALL/%sudo ALL=NOPASSWD:ALL/g' /etc/sudoers
 # Tweak sshd to prevent DNS resolution (speed up logins)
 echo 'UseDNS no' >> /etc/ssh/sshd_config
 # remove unsecure nist ecdsa keys
-sed -i /ecdsa/d' /etc/ssh/sshd_config
+sed -i '/ecdsa/d' /etc/ssh/sshd_config
 # i'm hesitant to include openssh-server from backports for wheezy, so commented out for now
 # HostKey /etc/ssh/ssh_host_ed25519_key
 
