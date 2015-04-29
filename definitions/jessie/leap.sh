2f37bc5b0c87f626c52bfbadebe10f4ec4a21803
varac <varacanero@zeromail.org>
2015-04-29 16:57:28 +0200

    ruby-hiera-puppet is not a deb package anymore but included into puppet

--- definitions/jessie/leap.sh
+++ definitions/jessie/leap.sh
@@ -1,7 +1,7 @@
 date > /etc/box_build_time
 
 # install puppet and other packages needed for leap_cli
-apt-get -y install puppet facter ruby-hiera-puppet rsync lsb-release
+apt-get -y install puppet rsync lsb-release
 # Install additional packages to vagrant wheezy basebox to make beaker happy #6898
 apt-get -y install ntpdate git rdoc
 
